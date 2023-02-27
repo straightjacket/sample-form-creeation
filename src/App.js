e2aa1de5bca034795d2e6f6b83a0cf52514faaad
Theron Blount <straightjacket@users.noreply.github.com>
2023-02-26 16:35:57 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import InputForm from "./InputForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <InputForm />
+    </div>
+  );
+}
---
+++ src/InputForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./styles.css";
+import DatePicker from "./DatePicker";
+
+class InputForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div className="InputForm">
+        <form>
+          <label>
+            Is going:
+            <input
+              name="isGoing"
+              type="checkbox"
+              checked={this.state.isGoing}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input
+              name="numberOfGuests"
+              type="number"
+              value={this.state.numberOfGuests}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Choose a date:
+            <DatePicker />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default InputForm;
